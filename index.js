30d6cb988c3768b14b06b127a267d688f836e921
Anton Shmigelskiy <bingsa@yandex.ru>
2022-06-07 22:46:00 +0300

    PORT

--- index.js
+++ index.js
@@ -140,9 +140,7 @@ app.post('/api/send/message/:telegramId/:chatId/:message', async (req, res) => {
     // })
 })
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT)
 
 
 bot.start(async (ctx) => {
